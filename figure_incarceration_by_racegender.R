c1d235640110acdf4359431bac5446c7bb8772db
crawleyliam <liam.crawley821@gmail.com>
2022-02-15 12:33:53 -0600

    Add script that outputs bargraph into plots

---
+++ figure_incarceration_by_racegender.R
@@ -0,0 +1,20 @@
+# Builds a bar graph with total arrests on the y axis and race/gender on the
+# x axis. Refer to my presentation on graphing for more detail.
+
+library(tidyverse)
+library(ggplot2)
+library(here)
+
+read_csv(here("data/NLSY97_clean.csv")) %>%
+  group_by(race, gender) %>%
+  summarize(total_incarcerations = mean(total_incarcerations)) %>%
+  ggplot(aes(race, total_incarcerations, fill = gender)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(
+    x = "Race", 
+    y = "Mean Incarcerations", 
+    fill = "Gender",
+    title = "Mean Number of Incarcerations in 2002 by Race and Gender") +
+  theme_minimal()
+
+ggsave(here("figures/incarcerations_by_racegender.png"), width=8, height=4.5)
