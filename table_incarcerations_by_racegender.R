0b0033a40355c8c1af8483c88e931f6c9100f3a6
crawleyliam <liam.crawley821@gmail.com>
2022-02-17 15:38:11 -0600

    modify title of incarc table

--- table_incarcerations_by_racegender.R
+++ table_incarcerations_by_racegender.R
@@ -15,7 +15,7 @@ read_csv(here("data/NLSY97_clean.csv")) %>%
   
   # create the kable object. Requires booktabs and float LaTeX packages
   kbl(
-    caption = "Mean incarcerations in 2002 by Race and Gender",
+    caption = "Mean Incarcerations in 2002 by Race and Gender",
     booktabs = TRUE,
     format = "latex",
     label = "tab:summarystats"
